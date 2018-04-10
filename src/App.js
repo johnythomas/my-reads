958e89ab01aa6bc2971cd0e1fd8fa7f848e7d811
johnythomas <johnythomas007@gmail.com>
2018-04-10 22:57:38 +0530

    feat: implement dynamic fetching of books using BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import ListBooks from "./components/ListBooks";
 import SearchBooks from "./components/SearchBooks";
 
-const BooksApp = () => (
-  <div className="app">
-    <Route exact path="/" component={ListBooks} />
-    <Route exact path="/search" component={SearchBooks} />
-  </div>
-);
+class BooksApp extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({
+        books
+      }));
+      console.log(books);
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" component={ListBooks} />
+        <Route exact path="/search" component={SearchBooks} />
+      </div>
+    );
+  }
+}
 
 export default BooksApp;
