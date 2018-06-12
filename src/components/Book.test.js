cc6d9070cb174bba934ad04be77104f088ccbcdf
Johny Thomas <johnythomas007@gmail.com>
2018-06-12 09:36:44 +0530

    Test: Add more test to Book component
    See Also: #4

--- src/components/Book.test.js
+++ src/components/Book.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Book from "./Book";
 import BookThumbnail from "../icons/BookThumbnail.png";
 
@@ -44,5 +44,55 @@ describe("Book", () => {
         .first()
         .props().style.backgroundImage
     ).toEqual(`url("${book.imageLinks.smallThumbnail}")`);
+  });
+
+  it("should display the book title passed as props", () => {
+    const wrapper = shallow(<Book book={book} onUpdateShelf={jest.fn()} />);
+    expect(
+      wrapper
+        .find(".book-title")
+        .first()
+        .text()
+    ).toEqual(book.title);
+  });
+
+  it("should provide link to the previewLink passed as props", () => {
+    const wrapper = shallow(<Book book={book} onUpdateShelf={jest.fn()} />);
+    expect(
+      wrapper
+        .find(".book-title")
+        .first()
+        .props().href
+    ).toEqual(book.previewLink);
+  });
+
+  it("should join and show the authors passed as props", () => {
+    book.authors = ["J.K Rowling", "Stephen King"];
+    const wrapper = shallow(<Book book={book} onUpdateShelf={jest.fn()} />);
+    expect(
+      wrapper
+        .find(".book-authors")
+        .first()
+        .text()
+    ).toEqual(book.authors.join());
+  });
+
+  it("should not display anything if author is not present", () => {
+    book.authors = null;
+    const wrapper = shallow(<Book book={book} onUpdateShelf={jest.fn()} />);
+    expect(
+      wrapper
+        .find(".book-authors")
+        .first()
+        .text()
+    ).toBe("");
+  });
+
+  it("should call the onUpdateShelf when shelf is changed", () => {
+    const onUpdateShelf = jest.fn();
+    const shelf = "currentlyReading";
+    const wrapper = mount(<Book book={book} onUpdateShelf={onUpdateShelf} />);
+    wrapper.find("select").simulate("change", { target: { value: shelf } });
+    expect(onUpdateShelf).toHaveBeenCalledWith(book, shelf);
   });
 });
